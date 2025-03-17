d6a95fd5230cb7f7fced0013a8402234de8a0550
Alina <aaetbaeva@uandex.ru>
2025-03-17 17:20:58 +0500

    h

--- HW hints/HW hints.sql
+++ HW hints/HW hints.sql
@@ -51,57 +51,53 @@ WHERE Inv.BillToCustomerID != ord.CustomerID
     AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
 GROUP BY ord.CustomerID, det.StockItemID
 ORDER BY ord.CustomerID, det.StockItemID
---Select * FROM Sales.OrderLines 
---Select * FROM Sales.Orders
---Select * FROM Sales.Invoices
---Select * FROM Sales.CustomerTransactions
---Select * FROM Warehouse.StockItemTransactions
+
 
 --др порядок в запросе, по принципу отфильтровать как можно больше записей. Изменение порядка таблиц дало хорошие показатели CPU time
---Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)    
---FROM Warehouse.StockItemTransactions AS ItemTrans
---    JOIN Sales.Invoices AS Inv 
---        ON Inv.InvoiceID = ItemTrans.InvoiceID
---    JOIN Sales.CustomerTransactions AS Trans
---        ON Trans.InvoiceID = Inv.InvoiceID
---	JOIN Sales.Orders AS ord
---		ON ord.OrderID = Inv.OrderID
---    JOIN Sales.OrderLines AS det
---        ON det.OrderID = ord.OrderID
---WHERE Inv.BillToCustomerID != ord.CustomerID
---    AND (Select SupplierId
---         FROM Warehouse.StockItems AS It
---         Where It.StockItemID = det.StockItemID) = 12
---    AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
---        FROM Sales.OrderLines AS Total
---            Join Sales.Orders AS ordTotal
---                On ordTotal.OrderID = Total.OrderID
---        WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
---    AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
---GROUP BY ord.CustomerID, det.StockItemID
---ORDER BY ord.CustomerID, det.StockItemID
+Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)    
+FROM Warehouse.StockItemTransactions AS ItemTrans
+    JOIN Sales.Invoices AS Inv 
+        ON Inv.InvoiceID = ItemTrans.InvoiceID
+    JOIN Sales.CustomerTransactions AS Trans
+        ON Trans.InvoiceID = Inv.InvoiceID
+	JOIN Sales.Orders AS ord
+		ON ord.OrderID = Inv.OrderID
+    JOIN Sales.OrderLines AS det
+        ON det.OrderID = ord.OrderID
+WHERE Inv.BillToCustomerID != ord.CustomerID
+    AND (Select SupplierId
+         FROM Warehouse.StockItems AS It
+         Where It.StockItemID = det.StockItemID) = 12
+    AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+        FROM Sales.OrderLines AS Total
+            Join Sales.Orders AS ordTotal
+                On ordTotal.OrderID = Total.OrderID
+        WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+    AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
 
 --изменение порядка запроса OPTION(FORCE ORDER) - порядок с самой маленькой таблицы. Это привело к появлению параллелизма и увеличению cost запроса, доптаблиц Worktable и Workfile
---Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)    
---FROM Sales.Orders AS ord
---    JOIN Sales.OrderLines AS det
---        ON det.OrderID = ord.OrderID
---    JOIN Sales.Invoices AS Inv 
---        ON Inv.OrderID = ord.OrderID
---    JOIN Sales.CustomerTransactions AS Trans
---        ON Trans.InvoiceID = Inv.InvoiceID
---    JOIN Warehouse.StockItemTransactions AS ItemTrans
---        ON ItemTrans.StockItemID = det.StockItemID
---WHERE Inv.BillToCustomerID != ord.CustomerID
---    AND (Select SupplierId
---         FROM Warehouse.StockItems AS It
---         Where It.StockItemID = det.StockItemID) = 12
---    AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
---        FROM Sales.OrderLines AS Total
---            Join Sales.Orders AS ordTotal
---                On ordTotal.OrderID = Total.OrderID
---        WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
---    AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
---GROUP BY ord.CustomerID, det.StockItemID
---ORDER BY ord.CustomerID, det.StockItemID
---OPTION(FORCE ORDER)
+Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)    
+FROM Sales.Orders AS ord
+    JOIN Sales.OrderLines AS det
+        ON det.OrderID = ord.OrderID
+    JOIN Sales.Invoices AS Inv 
+        ON Inv.OrderID = ord.OrderID
+    JOIN Sales.CustomerTransactions AS Trans
+        ON Trans.InvoiceID = Inv.InvoiceID
+    JOIN Warehouse.StockItemTransactions AS ItemTrans
+        ON ItemTrans.StockItemID = det.StockItemID
+WHERE Inv.BillToCustomerID != ord.CustomerID
+    AND (Select SupplierId
+         FROM Warehouse.StockItems AS It
+         Where It.StockItemID = det.StockItemID) = 12
+    AND (SELECT SUM(Total.UnitPrice*Total.Quantity)
+        FROM Sales.OrderLines AS Total
+            Join Sales.Orders AS ordTotal
+                On ordTotal.OrderID = Total.OrderID
+        WHERE ordTotal.CustomerID = Inv.CustomerID) > 250000
+    AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0
+GROUP BY ord.CustomerID, det.StockItemID
+ORDER BY ord.CustomerID, det.StockItemID
+OPTION(FORCE ORDER)
